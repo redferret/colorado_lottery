3498f6b85b4b454f55a64bba1dade28bf7b0c72c
Richard <rdesilvey@gmail.com>
2021-04-10 12:49:18 -0600

    Implement attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,11 @@
 class Contestant
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+
   def initialize(attrs)
-    
+    @first_name = attrs[:first_name]
+    @last_name = attrs[:last_name]
+    @age = attrs[:age]
+    @state_of_residence = attrs[:state_of_residence]
+    @spending_money = attrs[:spending_money]
   end
 end
