8599c1e49607304627237ae8e8c573e5c4de7453
Richard <rdesilvey@gmail.com>
2021-04-10 12:49:05 -0600

    Test full_name

--- spec/contestant_spec.rb
+++ spec/contestant_spec.rb
@@ -2,22 +2,22 @@ require 'rspec'
 require './lib/contestant'
 
 describe Contestant do
+  before :each do
+    @contestant = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigiades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+  end
   context '#initialize' do
-    before :each do
-      @contestant = Contestant.new({
-        first_name: 'Alexander',
-        last_name: 'Aigiades',
-        age: 28,
-        state_of_residence: 'CO',
-        spending_money: 10
-      })
-    end
     it 'exists' do
       expect(@contestant).to be_instance_of Contestant
     end
     it 'has a first and last name' do
       expect(@contestant.first_name).to eq "Alexander"
-      expect(@contestant.first_name).to eq "Aigiades"
+      expect(@contestant.last_name).to eq "Aigiades"
     end
     it 'has an age' do
       expect(@contestant.age).to eq 28
@@ -27,6 +27,12 @@ describe Contestant do
     end
     it 'has money to spend' do
       expect(@contestant.spending_money).to eq 10
+    end
+  end
+
+  context '#full_name' do
+    it 'shows full name' do
+      expect(@contestant.full_name).to eq "Alexander Aigiades"
     end
   end
 end
